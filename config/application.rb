fa7f60d56e26331a1b3c323957da1941d9e4a3f8
Alagu <allagappan@gmail.com>
2012-03-13 22:45:31 +0530

    include fonts

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,11 @@ module Radios
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-
+    
+    
+    # Asset search path - extra fonts.
+    config.assets.paths << Rails.root.join("app","assets","fonts")
+    
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
