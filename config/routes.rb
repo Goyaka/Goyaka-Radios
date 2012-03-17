b959ec440dbb3431ba75cd6ab237ba5d3b2c69aa
Alagu <allagappan@gmail.com>
2012-03-17 19:43:11 +0530

    Add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Radios::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/auth" }
   match '/new', :to =>'radios#new'
   match '/like', :to => 'radios#like'
-  match '/favicon.ico' :to => '/assets/favicon.ico'
   
   root :to => 'radios#index'
 end
