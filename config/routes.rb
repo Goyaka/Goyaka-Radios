f9664f25b103b2659c63867a91ae1823580063d5
Alagu <allagappan@gmail.com>
2012-03-17 19:39:40 +0530

    Merged

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Radios::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/auth" }
   match '/new', :to =>'radios#new'
   match '/like', :to => 'radios#like'
+  match '/favicon.ico' :to => '/assets/favicon.ico'
+  
   root :to => 'radios#index'
 end
