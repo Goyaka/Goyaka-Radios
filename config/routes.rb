678208c09596f5d121930a2fcb2bfae530da5b71
Alagu <allagappan@gmail.com>
2012-03-17 16:22:25 +0530

    Add like link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Radios::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/auth" }
   match '/new', :to =>'radios#new'
+  match '/like', :to => 'radios#like'
   root :to => 'radios#index'
 end
