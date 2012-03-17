c6058f3e39c9a33cf73b304355f3b6c37715ac66
Alagu <allagappan@gmail.com>
2012-03-17 16:21:18 +0530

    Send status as OK, when liked.

--- app/controllers/radios_controller.rb
+++ app/controllers/radios_controller.rb
@@ -8,9 +8,13 @@ class RadiosController < ApplicationController
     submissions  = Submission.all(sort: [[ :created, :desc ]]).limit(20)
     @submissions = Submission.youtube_filter submissions
   end
-  
+
   def like
     submission = Submission.where(:post_id => params[:post_id]).first
-    submission.like.push current_user
+    post = FbGraph::Post.new(params[:post_id])
+    post.like!(:access_token => current_user[:credentials]['token'])
+    submission.add_user_like current_user
+    
+    render :json => {'status' => 'OK'}
   end
 end
